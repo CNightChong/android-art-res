5805e6dd5783e44d4d2df5e201f0bd4fecd6e8f7
Chong <cnightchong@gmail.com>
2016-07-27 08:58:35 +0800

    Android studio project

---
+++ Chapter07/app/src/main/java/com/chong/chapter07/TestActivity.java
@@ -0,0 +1,27 @@
+package com.chong.chapter07;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+    private static final String TAG = "TestActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.enter_anim, R.anim.exit_anim);
+    }
+
+}
