9d1644a9fc0fa395ef79dfbaad3a34a34d03de52
Chong <cnightchong@gmail.com>
2016-07-31 11:42:16 +0800

    Android Studio project

--- Chapter_13/MultiDexTest/app/src/main/java/com/ryg/multidextest/ui/View1.java
+++
@@ -1,9 +0,0 @@
-package com.ryg.multidextest.ui;
-
-/**
- * Created by renyugang on 15-5-27.
- */
-public class View1 {
-    int a;
-    String b;
-}
--- Chapter_13/MultiDexTest/app/src/main/java/com/ryg/multidextest/ui/View2.java
+++
@@ -1,8 +0,0 @@
-package com.ryg.multidextest.ui;
-
-/**
- * Created by renyugang on 15-5-27.
- */
-public class View2 {
-    public static String msg = "helloworld";
-}
